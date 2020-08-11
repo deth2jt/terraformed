03152350d68915ccbe3c261e8e0b8b1ab7f011ae
Jerome Thompson <jthompson@rhythmictech.com>
2020-08-11 00:25:51 -0400

    switch default gateway

--- vpc/main.tf
+++ vpc/main.tf
@@ -25,24 +25,6 @@ resource "aws_internet_gateway" "gw" {
 }
 
 
-resource "aws_route_table" "public_route" {
-	vpc_id = "${aws_vpc.main.id}"
-
-	route {
-		cidr_block = "0.0.0.0/0"
-		gateway_id = "${aws_internet_gateway.gw.id}"
-	}
-
-	tags = {
-		Name = "foo-terraform-rt"
-	}
-}
-
-resource "aws_eip" "my-foo-eip" {
-	vpc = true
-}
-
-
 resource "aws_subnet" "public_subnet" {
 	count = 2
 	cidr_block = "${var.public_cidrs[count.index]}"
@@ -56,26 +38,15 @@ resource "aws_subnet" "public_subnet" {
   	}
 }
 
+resource "aws_eip" "my-foo-eip" {
+  vpc = true
+}
+
+
 resource "aws_nat_gateway" "my-foo-nat-gateway"{
 	allocation_id = "${aws_eip.my-foo-eip.id}"
 	subnet_id = "${aws_subnet.public_subnet.0.id}"
 }
-
-
-resource "aws_default_route_table" "private_route" {
-	default_route_table_id = "${aws_vpc.main.default_route_table_id}"
-
-	route { 
-		nat_gateway_id = "${ aws_nat_gateway.my-foo-nat-gateway.id }"
-		cidr_block = "0.0.0.0/0"
-	}
-
-	tags = {
-		Name = "my-private-route-table"
-	}
-}
-
-
 
 
 
@@ -135,4 +106,37 @@ resource "aws_route" "my-tgw-route" {
   route_table_id         = "${aws_route_table.public_route.id}"
   destination_cidr_block = "0.0.0.0/0"
   transit_gateway_id     = "${var.transit_gateway}"
+}
+
+
+#resource "aws_route_table" "public_route" {
+resource "aws_default_route_table" "private_route" {
+    default_route_table_id = "${aws_vpc.main.default_route_table_id}"
+    
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.gw.id}"
+    }
+
+    tags = {
+        Name = "foo-terraform-rt"
+    }
+}
+
+
+
+
+
+resource "aws_route_table" "public_route" {
+    vpc_id = "${aws_vpc.main.id}"
+
+    route { 
+        nat_gateway_id = "${ aws_nat_gateway.my-foo-nat-gateway.id }"
+        cidr_block = "0.0.0.0/0"
+    }
+
+    tags = {
+        Name = "my-private-route-table"
+    }
 }
